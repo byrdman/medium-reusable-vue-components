a49c2e4445bf1dc169d32b19415f140ffdc94964
Sam Byrd <sbyrd@thebyrdnest.net>
2019-05-08 21:02:42 -0500

    after some work

---
+++ components/JTableRow/JTableRow.vue
@@ -0,0 +1,19 @@
+<template>
+    <tr>
+        <td v-for="(value, index) in values" :key="index">
+            {{ value }}
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: 'JTableRow',
+        props: {
+            values: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
