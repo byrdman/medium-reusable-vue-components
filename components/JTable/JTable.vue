8217044015d211c6b0312d162045dcd199b1d84c
Sam Byrd <sbyrd@thebyrdnest.net>
2019-05-08 21:29:25 -0500

    added header prop to JTable

--- components/JTable/JTable.vue
+++ components/JTable/JTable.vue
@@ -1,5 +1,13 @@
 <template>
     <table>
+        <tr>
+            <th
+                v-for="(label, index) in header"
+                :key="index"
+                >
+                {{ label }}
+            </th>
+        </tr>
         <JTableRow
             v-for="(values, index) in items"
             :key="index"
@@ -12,6 +20,9 @@
     export default {
         name: 'JTable',
         props: {
+            header: {
+                type: Array
+            },
             items: {
                 type: Array,
                 required: true
@@ -20,3 +31,12 @@
         components: {JTableRow}
     }
 </script>
+
+<style>
+    th {
+        text-align: left;
+        padding: 8px;
+        font-size: 13px;
+        color: #8d8d92;
+    }
+</style>
